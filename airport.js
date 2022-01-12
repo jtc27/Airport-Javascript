aed7b980572e142d922a91cd6890e0c0ff52dead
Jacob Cho <jacobcho@live.com>
2022-01-12 20:10:40 +0000

    addPlane method

--- airport.js
+++ airport.js
@@ -1,10 +1,15 @@
 class Airport {
   constructor(name) {
-    this.name = name
+    this.name = name,
+    this.hangar = []
   }
 
   sayHello(){
     console.log('This airport is called ' + this.name)
+  }
+
+  addPlane(plane){
+    this.hangar.push(plane)
   }
 
 }
@@ -14,5 +19,13 @@ let heathrow = new Airport('Heathrow');
 console.log(heathrow)
 console.log(heathrow.name)
 console.log(heathrow.sayHello())
+
+console.log(heathrow.hangar)
+console.log(heathrow.hangar.length)
+
+heathrow.addPlane('Airbus 920')
+
+console.log(heathrow.hangar)
+console.log(heathrow.hangar.length)
 
 module.exports = Airport
--- airport.test.js
+++ airport.test.js
@@ -4,4 +4,6 @@ test('new airport is called Heathrow', () => {
   let heathrow = new Airport('Heathrow');
 
   expect(heathrow.name).toEqual('Heathrow')  
+  expect(heathrow.name).not.toBe('JFK')  
 })
+
